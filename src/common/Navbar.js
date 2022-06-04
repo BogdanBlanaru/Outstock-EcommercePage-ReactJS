4f4b64887f345d6b0debc4b8ecb47becf953577a
BogdanBlanaru <bogdansebastian200@yahoo.com>
2022-06-05 01:20:40 +0300

    5iunie

--- src/common/Navbar.js
+++ src/common/Navbar.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./Navbar.css";
 import Logo from "../assets/logo.png";
+import BurgerMenu from "../assets/burgerMenu.png";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
 	const [values, setValues] = useState([]);
 	const [valuesCart, setValuesCart] = useState([]);
+
+	const [showBurgerMenu, setShowBurgerMenu] = useState(false);
 
 	useEffect(() => {
 		const productListStorage = localStorage.getItem("productList");
@@ -24,32 +27,73 @@ const Navbar = () => {
 	}, []);
 
 	return (
-		<div className='navbar'>
-			<Link className='links' to='/'>
-				Home
-			</Link>
-			<Link className='links' to='/products'>
-				Products
-			</Link>
+		<>
+			<div className='navbar'>
+				<Link className='links responsive_linksleft' to='/'>
+					Home
+				</Link>
+				<Link className='links responsive_linksleft' to='/products'>
+					Products
+				</Link>
 
-			<Link className='links' to='/aboutus'>
-				About Us
-			</Link>
+				<Link className='links responsive_linksleft' to='/aboutus'>
+					About Us
+				</Link>
 
-			<Link className='links' to='/'>
-				<img className='style_logo' src={Logo} />
-			</Link>
+				<Link className='links responsive_logo' to='/'>
+					<img className='style_logo' src={Logo} />
+				</Link>
 
-			<Link className='links' to='/wishlist'>
-				Wishlist({values.length})
-			</Link>
-			<Link className='links' to='/cart'>
-				Cart({valuesCart.length})
-			</Link>
-			<Link className='links' to='/checkout'>
-				Checkout
-			</Link>
-		</div>
+				<Link className='links responsive_linksright' to='/wishlist'>
+					Wishlist({values.length})
+				</Link>
+				<Link className='links responsive_linksright' to='/cart'>
+					Cart({valuesCart.length})
+				</Link>
+				<Link className='links responsive_linksright' to='/checkout'>
+					Checkout
+				</Link>
+			</div>
+
+			<div className='mobile_nav'>
+				<Link className='mobile_logo' to='/'>
+					<img className='style_logo' src={Logo} />
+				</Link>
+				<img
+					src={BurgerMenu}
+					alt='Burger Menu'
+					className='burger_menu'
+					onClick={() => {
+						setShowBurgerMenu((prevShowBurgerMenu) => !prevShowBurgerMenu);
+					}}
+				/>
+			</div>
+
+			{showBurgerMenu && (
+				<div className='mobile_menu'>
+					<Link className='links ' to='/'>
+						Home
+					</Link>
+					<Link className='links ' to='/products'>
+						Products
+					</Link>
+
+					<Link className='links ' to='/aboutus'>
+						About Us
+					</Link>
+
+					<Link className='links ' to='/wishlist'>
+						Wishlist({values.length})
+					</Link>
+					<Link className='links ' to='/cart'>
+						Cart({valuesCart.length})
+					</Link>
+					<Link className='links' to='/checkout'>
+						Checkout
+					</Link>
+				</div>
+			)}
+		</>
 	);
 };
 
